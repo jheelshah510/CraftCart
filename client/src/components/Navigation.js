9d5e4899ccab59e9d24dc21908f0449d6739c5b5
JaneshVyas <97336183+JaneshVyas@users.noreply.github.com>
2023-01-11 00:14:38 +0530

    navbar-changes

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,56 +1,54 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import logo from '../CraftCart.png'
 
 const Navigation = () => {
   return (
     <>
-    <Navbar expand="lg" style={{backgroundColor: "#a8d3ed"}}>
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Form className="d-flex">
-            <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-            <Nav.Link href="#action1">Products</Nav.Link>
-            <Nav.Link href="#action2">Deals</Nav.Link>
-            <Nav.Link href="#action6">
-            <FontAwesomeIcon icon={faCartShopping} /> Cart</Nav.Link>
-            <NavDropdown title="Login" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-      </Navbar.Collapse>
+    <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" style={{fontSize: 20}}>
+      <Container>
+        <Navbar.Brand href="/" style={{fontSize: 30, fontFamily: 'cursive'}}><img src={logo} alt='logo' /> CraftCart</Navbar.Brand>
 
-    </Container>
-  </Navbar>
-  
-  </>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="m-auto">
+              <Form inline>
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                />
+              </Form>
+          </Nav>
+          <Nav className="mr-sm-4">
+              <>
+                <Nav.Link href="#">Login</Nav.Link>
+              </>
+          </Nav>
+          <Nav className="mr-sm-4">
+              <>
+                <Nav.Link href="#"><FontAwesomeIcon icon={faCartShopping} />Cart</Nav.Link>
+              </>
+          </Nav>
+          <Nav className="mr-sm-4">
+              <>
+                <Nav.Link href="#">Products</Nav.Link>
+              </>
+          </Nav>
+          <Nav className="mr-sm-4">
+              <>
+                <Nav.Link href="#">Deals</Nav.Link>
+              </>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
   )
 }
 
