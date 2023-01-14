8e318a10706c1afe534b24193b2b911fe6836716
Jheel Shah <jheelshah510@gmail.com>
2023-01-14 22:34:01 +0530

    env setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+app.get("/home", (req, res) => {
+  res.send("homepage");
+});
+
+app.listen(3030, () => {
+  console.log("listening");
+});
