baae94585e0362ef6107642da5a87c757bfe712f
Jheel Shah <jheelshah510@gmail.com>
2023-01-15 22:12:34 +0530

    modifieed credentials

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
-const MONGO_URL =
-  "mongodb+srv://jheelshah510:20cs082@charusat.edu.in@cluster0.3k5m4vd.mongodb.net/?retryWrites=true&w=majority";
-
+const MONGO_URL = `mongodb+srv://${process.env.DB_name}:${process.env.DB_password}@cluster0.3k5m4vd.mongodb.net/?retryWrites=true&w=majority`;
+console.log(MONGO_URL);
 const PORT = 3000;
 
 app.listen(PORT, () => {
