ea25ff48767bea4fbc1880e5632d4dd8230a4d49
JaneshVyas <20cs102@charusat.edu.in>
2023-02-13 15:04:43 +0530

    Seller registration

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import sellersignIn from "./pages/Sellersignin";
 import Register from "./pages/Register";
 import ProductDetails from "./pages/ProductDetails";
+import SellerRegistration from "./pages/SellerRegistration";
 
 axios.defaults.withCredentials = true;
 
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/sellersignin" component={sellersignIn} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/productdetails" component={ProductDetails} />
+          <Route exact path="/sellerregistration" component={SellerRegistration} />
         </BrowserRouter>
       </AuthContextProvider>
     </>
