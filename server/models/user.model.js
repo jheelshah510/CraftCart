375d7a135dc65a3c3dc51d8faf99d1ab96ded09b
Jheel Shah <jheelshah510@gmail.com>
2023-03-20 16:06:00 +0530

    model and images added

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -5,6 +5,9 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   passwordHash: { type: String, required: true },
   role: { type: String, default: "buyer", required: true },
+  address: { type: String, required: true, default: " " },
+  pincode: { type: Number, required: true, default: " " },
+  phoneNumber: { type: String, required: true, default: " " },
 });
 
 const User = mongoose.model("users", userSchema);
