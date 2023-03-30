f9375ce50d61dfbebb4fc2fff3895407f23c0bf1
Jheel Shah <jheelshah510@gmail.com>
2023-03-30 22:32:42 +0530

    filter category

---
+++ server/controllers/cart.controller.js
@@ -0,0 +1,6 @@
+// const Cart = require("../models/cart.model");
+// const mongoose = require("mongoose");
+
+// exports.addCart = (req, res) => {
+//   const userId = req.body;
+// };
---
+++ server/routers/cartRouter.js
@@ -0,0 +1,7 @@
+// const router = require("express").Router();
+
+// const { addCart } = require("../controllers/cart.controller");
+
+// router.post("/addToCart", addCart);
+
+// module.exports = router;
