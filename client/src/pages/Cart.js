332e9700a924f7338147799afe1b4eebd826119c
Jheel Shah <jheelshah510@gmail.com>
2023-03-31 20:39:54 +0530

    cart added successfully

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,130 +1,232 @@
-import React, { useState } from "react";
+import React from "react";
 import Navigation from "../components/Navigation";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMinus,
+  faPlus,
+  faExclamationTriangle,
+} from "@fortawesome/free-solid-svg-icons";
 import { useCartContext } from "../context/CartContext";
 import { useHistory } from "react-router-dom";
 import Card from "react-bootstrap/Card";
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
+import UserInfoContext from "../context/UserInfoContext";
+import { useState } from "react";
+import Loading from "../components/Loading";
 
 const Cart = () => {
-  const [amount, setAmount] = useState(1);
-  const { cart, removeItem, setDecrease, setIncrement } = useCartContext();
+  const { cart, removeItem, setDecrease, setIncrement, total_price } =
+    useCartContext();
   const history = useHistory();
-  console.log(cart);
 
-  if (cart.length === 0) {
-    return (
-      <div>
-        <h3>Your Cart is empty</h3>
+  const { loggedIn } = useContext(AuthContext);
+  const allData = useContext(UserInfoContext);
+  const [isLoaded, setIsLoaded] = useState(false);
+  setTimeout(() => {
+    setIsLoaded(true);
+  }, 1000);
 
-        <Button
-          variant="outline-primary"
-          type="submit"
-          onClick={() => history.push("/searchproduct")}
-        >
-          {" "}
-          Continue Shopping{" "}
-        </Button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <Navigation />
-        <Card style={{ marginTop: "1%", marginLeft: "5%", width: "50%" }} body>
-          This is some text within a card body.
-        </Card>
-        ;
-        <div style={{ display: "flex" }}>
-          <div
-            style={{
-              marginLeft: "5%",
-              marginRight: "2%",
-              marginTop: "1%",
-              width: "80%",
-            }}
+  // return a loading message if isLoaded is false
+  if (!isLoaded) {
+    return <Loading />;
+  }
+  console.log(allData);
+
+  if (loggedIn) {
+    if (cart.length === 0) {
+      return (
+        <div>
+          <h3>Your Cart is empty</h3>
+
+          <Button
+            variant="outline-primary"
+            type="submit"
+            onClick={() => history.push("/searchproduct")}
           >
-            {cart.map((currElem) => {
-              return (
-                <div
-                  className="singleproduct"
-                  style={{
-                    display: "flex",
-                    border: "1px solid black",
-                    width: "90%",
-                    marginBottom: "1%",
-                  }}
+            {" "}
+            Continue Shopping{" "}
+          </Button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Navigation />
+          <Card
+            style={{ marginTop: "1%", marginLeft: "5%", width: "50%" }}
+            body
+          >
+            {allData.address.length > 0 &&
+              allData.phoneNumber.length > 0 &&
+              allData.pincode && (
+                <>
+                  <b></b>Deliver to: {allData.address}, {allData.pincode}
+                  <br />
+                  Phone number: {allData.phoneNumber}
+                </>
+              )}
+            {allData.address.length === 0 ||
+            allData.phoneNumber.length === 0 ||
+            allData.pincode.toString().length === null ? (
+              <>
+                {<FontAwesomeIcon icon={faExclamationTriangle} />} &nbsp;
+                &nbsp;Please Update your User Profile to order. Click{" "}
+                <a
+                  href={"/profile/" + allData._id}
+                  style={{ color: "blue", textDecoration: "underline" }}
                 >
-                  <div>
-                    <img
-                      src={currElem.image}
-                      style={{ width: "15vw", height: "20vh", margin: "5px" }}
-                      alt="hello"
-                    />
-                    <div
-                      className="increase-decrease"
-                      style={{ marginLeft: "30%", marginBottom: "1%" }}
-                    >
-                      <button onClick={() => setDecrease(currElem.id)}>
-                        <FontAwesomeIcon icon={faMinus} />
-                      </button>
-                      <h4 style={{ margin: "2px" }}>{currElem.amount}</h4>
-                      <button onClick={() => setIncrement(currElem.id)}>
-                        <FontAwesomeIcon icon={faPlus} />
-                      </button>
+                  here
+                </a>
+                &nbsp;to update
+              </>
+            ) : null}
+          </Card>
+
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                marginLeft: "5%",
+                marginRight: "2%",
+                marginTop: "1%",
+                width: "80%",
+              }}
+            >
+              {cart.map((currElem) => {
+                return (
+                  <div
+                    className="singleproduct"
+                    style={{
+                      display: "flex",
+                      border: "1px solid black",
+                      width: "90%",
+                      marginBottom: "3%",
+                    }}
+                  >
+                    <div>
+                      <img
+                        src={currElem.image}
+                        style={{ width: "15vw", height: "20vh", margin: "5px" }}
+                        alt="hello"
+                      />
+                      <div
+                        className="increase-decrease"
+                        style={{ marginLeft: "30%", marginBottom: "1%" }}
+                      >
+                        <button onClick={() => setDecrease(currElem.id)}>
+                          <FontAwesomeIcon icon={faMinus} />
+                        </button>
+                        <h4 style={{ margin: "2px" }}>{currElem.amount}</h4>
+                        <button onClick={() => setIncrement(currElem.id)}>
+                          <FontAwesomeIcon icon={faPlus} />
+                        </button>
+                      </div>
+                    </div>
+                    <div>
+                      <h3>{currElem.name}</h3>
+
+                      <h4>&#x20B9;{currElem.price * currElem.amount}</h4>
+
+                      <Button
+                        variant="outline-danger"
+                        onClick={() => removeItem(currElem.id)}
+                      >
+                        {" "}
+                        Remove{" "}
+                      </Button>
                     </div>
                   </div>
-                  <div>
-                    <h3>{currElem.name}</h3>
+                );
+              })}
 
-                    <h4>{currElem.price * currElem.amount}</h4>
-
-                    <Button
-                      variant="outline-danger"
-                      onClick={() => removeItem(currElem.id)}
-                    >
-                      {" "}
-                      Remove{" "}
-                    </Button>
-                  </div>
-                </div>
-              );
-            })}
-
-            <Button
-              variant="outline-primary"
-              type="submit"
-              onClick={() => history.push("/searchproduct")}
+              <Button
+                variant="info"
+                type="submit"
+                onClick={() => history.push("/searchproduct")}
+              >
+                {" "}
+                Continue Shopping{" "}
+              </Button>
+            </div>
+            <div
+              style={{
+                width: "40%",
+                height: "30%",
+                margin: "5%",
+                marginTop: "1%",
+              }}
             >
-              {" "}
-              Continue Shopping{" "}
-            </Button>
-          </div>
-          <div
-            style={{
-              border: "1px solid black",
-              width: "40%",
-              height: "30%",
-              margin: "5%",
-              marginTop: "1%",
-            }}
-          >
-            <div style={{ margin: "2%" }}>
-              <h3>PRICE DETAILS</h3>
-              <br />
-              <h4>Price - $300</h4>
-              <br />
-              <h4>Discount - $100</h4>
-              <br />
-              <h4>
-                <b>Total Amount - $200</b>
-              </h4>
+              {/* <div style={{ margin: "2%" }}>
+                <h3>PRICE DETAILS</h3>
+                <br />
+                <h4>&#x20B9;{total_price}</h4>
+                <br />
+                <h4>GST-&#x20B9;{0.05 * total_price}</h4>
+                <br />
+                <h4>
+                  <b>Total Amount - {total_price + 0.05 * total_price}</b>
+                </h4>
+              </div> */}
+              <Card
+                style={{
+                  width: "18rem",
+                  height: "15rem",
+                  border: "1px groove black",
+                }}
+              >
+                <Card.Header>Price Details</Card.Header>
+                <Card.Body>
+                  Price: &#x20B9;{total_price}
+                  <br />
+                  <br />
+                  GST-&#x20B9;{0.05 * total_price}
+                  <br />
+                </Card.Body>
+                <Card.Footer>
+                  {" "}
+                  Total Amount - {total_price + 0.05 * total_price}
+                </Card.Footer>
+              </Card>
+              {allData.address.length > 0 &&
+                allData.phoneNumber.length > 0 &&
+                allData.pincode && (
+                  <Button
+                    variant="primary"
+                    style={{
+                      width: "10vw",
+                      height: "6vh",
+                      marginTop: "20px",
+                      marginLeft: "4vw",
+                    }}
+                  >
+                    Place Order
+                  </Button>
+                )}
+
+              {allData.address.length === 0 ||
+              allData.phoneNumber.length === 0 ||
+              allData.pincode.toString().length === 0 ? (
+                <Button
+                  variant="primary"
+                  style={{
+                    width: "10vw",
+                    height: "6vh",
+                    marginTop: "20px",
+                    marginLeft: "4vw",
+                  }}
+                  disabled
+                >
+                  Place Order
+                </Button>
+              ) : null}
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+  } else {
+    return <div>Please Login to View Cart</div>;
   }
 };
 
